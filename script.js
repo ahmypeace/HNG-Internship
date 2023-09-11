fbc0afecd68292e6411ed5d679e9d2c5cf0976d8
ahmypeace <50794990+ahmypeace@users.noreply.github.com>
2023-09-11 08:19:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
-// Getting elements by their data-testid attributes
-const slackUserName = document.querySelector('[data-testid="Amaka Peace"]');
+// Get elements by their data-testid attributes
+const slackUserName = document.querySelector('[data-testid="slackUserName"]');
 const currentDayOfTheWeek = document.querySelector('[data-testid="currentDayOfTheWeek"]');
 const currentUTCTime = document.querySelector('[data-testid="currentUTCTime"]');
 
-// My Slack name
+// Set your Slack name
 slackUserName.textContent = "Amaka Peace";
 
-// Getting the current date
-const currentDate = new Date();
+// Function to update the current day of the week
+function updateDayOfWeek() {
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const currentDate = new Date();
+    const dayIndex = currentDate.getDay();
+    currentDayOfTheWeek.textContent = daysOfWeek[dayIndex];
+}
 
-// Setting the current day of the week
-const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const dayIndex = currentDate.getDay();
-currentDayOfTheWeek.textContent = daysOfWeek[dayIndex];
+// Function to update the current UTC time in milliseconds
+function updateUTCTime() {
+    const currentDate = new Date();
+    currentUTCTime.textContent = currentDate.getTime();
+}
 
-// Getting the current UTC time in milliseconds
-currentUTCTime.textContent = currentDate.getTime();
+// Update the day of the week and UTC time initially and then every second
+updateDayOfWeek();
+updateUTCTime();
+setInterval(updateDayOfWeek, 1000); // Update every second
+setInterval(updateUTCTime, 1000);   // Update every second
